ddc613635444b40d2aa1126722dcd990829cc2c5
Wahyu Permadi <wahyu.permadi1725@gmail.com>
2019-09-07 01:15:15 +0700

    [ADD] modul data, common

--- buildSrc/src/main/java/Depedencies.kt
+++ buildSrc/src/main/java/Depedencies.kt
@@ -14,6 +14,8 @@ object Modules {
     const val remote = ":data:remote"
     const val repository = ":data:repository"
     const val common = ":common"
+    const val movies = ":feature:movies"
+    const val tvShow = ":feature:tvshow"
 }
 
 object Versions {
@@ -99,25 +101,4 @@ object AndroidLibraries {
 object KotlinLibraries {
     const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
     const val kotlinCoroutineCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}"
-}
-
-object TestLibraries {
-    // ANDROID TEST
-    const val androidTestRunner = "androidx.test:runner:${Versions.androidTestRunner}"
-    const val espresso = "androidx.test.espresso:espresso-core:${Versions.espressoCore}"
-    const val espressoContrib = "androidx.test.espresso:espresso-contrib:${Versions.espressoCore}"
-    const val archCoreTest = "androidx.arch.core:core-testing:${Versions.archCoreTest}"
-    const val junit = "androidx.test.ext:junit:${Versions.androidJunit}"
-    const val fragmentNav = "androidx.fragment:fragment-testing:${Versions.fragmentTest}"
-    // KOIN
-    const val koin = "org.koin:koin-test:${Versions.koin}"
-    // MOCK WEBSERVER
-    const val mockWebServer = "com.squareup.okhttp3:mockwebserver:${Versions.okHttp}"
-    // MOCK
-    const val mockkAndroid = "io.mockk:mockk-android:${Versions.mockk}"
-    const val mockk = "io.mockk:mockk:${Versions.mockk}"
-    // COROUTINE
-    const val coroutine = "org.jetbrains.kotlinx:kotlinx-coroutines-test:${Versions.coroutines}"
-    // DATA BINDING
-    const val databinding = "androidx.databinding:databinding-compiler:${Versions.databinding}"
 }
